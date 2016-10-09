224ba84dae941b2566bcb5dc307e2451f13e4e04
Borin Ouch <borin.ouch@gmail.com>
2016-10-09 09:05:24 -0700

    Add pascal grammar rules and example program

--- part009.cpp
+++ part009.cpp
@@ -3,6 +3,44 @@
 #include <sstream>
 #include <memory>
 
+/*
+ * PASCAL GRAMMAR RULES
+ *
+ * program: compound_statement DOT
+ * compound_statement: BEGIN statement_list END
+ * statement_list: statement
+ *               | statement SEMI statement_list
+ * statement: compound_statement
+ *          | assignment_statement
+ *          | empty
+ * assignment_statement: variable ASSIGN expr
+ * empty:
+ * expr: term ((PLUS | MINUS) term)*
+ * term: factor ((MUL | DIV) factor)*
+ * factor: PLUS factor
+ *       | MINUS factor
+ *       | INTEGER
+ *       | LPAREN expr RPAREN
+ *       | variable
+ * variable: ID
+ *
+ */
+
+/*
+ * EXAMPLE PASCAL PROGRAM
+ *
+ * BEGIN
+ *   BEGIN
+ *     number := 2;
+ *     a := number;
+ *     b := 10 * a + 10 * number / 4;
+ *     c := a - - b;
+ *   END;
+ *   x := 11;
+ * END.
+ *
+ */
+
 struct Tokens {
     enum Type {
         Integer,
